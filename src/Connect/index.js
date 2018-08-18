139d402f8032f1bf761c73388d1eb553005646c8
Alexander Schelchere <ascellie@gmail.com>
2018-08-17 21:39:28 -0400

    connect page is more presentable

--- src/Connect/index.js
+++ src/Connect/index.js
@@ -8,31 +8,26 @@ class Connect extends Component {
     this.state = {
       interwebs: [
         {
-          name: "LinkedIn",
+          name: `LinkedIn's Like... Really "In" now.`,
           link: "https://www.linkedin.com/in/alexanderschelchere/"
         },
         {
-          name: "GitHub",
+          name: "Gotta Commit. Onwards to GitHub!",
           link: "https://github.com/Lexscher"
         },
         {
-          name: "Twitter",
+          name: "That one time I wrote something on Medium.",
+          link: "https://medium.com/@ascellie"
+        },
+        {
+          name: "@ me on Twitter!",
           link: "https://twitter.com/Lexscher"
         },
         {
-          name: "Resume",
+          name: "My Résumé.",
           link:
             "https://drive.google.com/open?id=1dw9emPfyyUvsLqfZ5WbA2GZ-LnWdODH-"
         }
-      ],
-      skills: [
-        "JavaScript",
-        "Ruby on Rails",
-        "React",
-        "React Native",
-        "Node.js",
-        "Git",
-        "Express"
       ]
     };
   }
@@ -49,18 +44,13 @@ class Connect extends Component {
         <h3>{link.name}</h3>
       </a>
     ));
-    let skillsList = this.state.skills.map((skill, index) => (
-      <p className="skill-item" key={index}>
-        {skill}
-      </p>
-    ));
     return (
       <div className="connect">
         <h1>Glad you were intrested enough to connect with me!</h1>
-        <h2>Find me on the internet.</h2>
+        <h2>
+          <u>Find me on the internet.</u>
+        </h2>
         <div className="social-media">{socials}</div>
-        <h2>A few of my skills...</h2>
-        <ul className="skill-item-container">{skillsList}</ul>
       </div>
     );
   }

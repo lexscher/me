321968945054789e1f62eb2b7a63380ac4f3ae68
Alexander Schelchere <ascellie@gmail.com>
2018-09-01 23:40:40 -0400

    working on the hidden contact form

--- src/Connect/index.js
+++ src/Connect/index.js
@@ -28,8 +28,31 @@ class Connect extends Component {
           link:
             "https://drive.google.com/open?id=16fGCDC3jH5FJ7fyre-O0NQOb69qHyJYm"
         }
-      ]
+      ],
+      name: "",
+      email: "",
+      message: "",
     };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+
+  }
+
+  handleNameChange(event) {
+    console.log(event.target.value);
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange(event) {
+    console.log(event.target.value);
+    this.setState({email: event.target.value});
+  }
+
+  handleMessageChange(event) {
+    console.log(event.target.value);
+    this.setState({message: event.target.value});
   }
 
   render() {
@@ -50,7 +73,26 @@ class Connect extends Component {
         <h2>
           <u>Find me on the internet.</u>
         </h2>
-        <div className="social-media">{socials}</div>
+        <div className="social-media">
+          {socials}
+          <h3 id="contact-me" className="about-link">
+            You can email me, too!
+          </h3>
+        </div>
+        <div className="form-container">
+          <form
+            action="mailto:ascellie@gmail.com"
+            method="post"
+            enctype="text/plain"
+          >
+          
+            <input className="form-info" type="text" name="name" placeholder="Name" value={this.state.value} onChange={this.handleNameChange} />
+            <input className="form-info" type="text" name="mail" placeholder="E-Mail" value={this.state.value} onChange={this.handleEmailChange} />
+            <textarea className="form-message" name="comment" rows="7" cols="48" placeholder="Write your message here..." value={this.state.value} onChange={this.handleMessageChange} />
+            <input className="form-button" type="submit" value="submit" />
+            
+          </form>
+        </div>
       </div>
     );
   }

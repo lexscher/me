7757dfbd5b8b525e1f3bed1f896f31f50cd68536
Alexander Schelchere <ascellie@gmail.com>
2018-09-27 22:48:07 -0400

    basic skeleton

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 // Import another component - flip card game to let the user find out about them.
+import CardGame from "../CardGame";
 // Content about me
 class Home extends Component {
   render() {
-    return <h1>Home.</h1>;
+    return (
+      <div>
+        <h1>Home.</h1>
+        <CardGame />
+      </div>
+    );
   }
 }
 

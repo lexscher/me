eca8fc4863a1ebc288d35ef81ba85f86c0879dbb
Alexander Schelchere <ascellie@gmail.com>
2019-08-16 01:18:07 -0400

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import Connect from "./Connect";
-import About from "./About";
-import Portfolio from "./Portfolio";
-import Game from "./Game";
-import Sorry from "./Sorry";
-
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Connect from './Connect';
+import About from './About';
+import Portfolio from './Portfolio';
+import Game from './Game';
+import Sorry from './Sorry';
 
 class App extends Component {
   render() {
-  let toTheTop = () => {
-    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (currentScroll > 0) {
-         window.requestAnimationFrame(toTheTop);
-         window.scrollTo (0,currentScroll - (currentScroll/5));
-    }
-  };
+    let toTheTop = () => {
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(toTheTop);
+        window.scrollTo(0, currentScroll - currentScroll / 5);
+      }
+    };
     return (
       <Router>
         <div className="container">
@@ -30,9 +29,9 @@ class App extends Component {
             <Link className="links" to="/connect" onClick={toTheTop}>
               Connect!
             </Link>
-            <Link className="links" to="/game">
+            {/* <Link className="links" to="/game">
               Play a Game?!
-            </Link>
+            </Link> */}
           </nav>
           <div className="content">
             <Switch>

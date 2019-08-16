0aca483d480c8943cc8b068025d0447a8ee353c6
Alexander Schelchere <ascellie@gmail.com>
2019-08-16 00:48:51 -0400

    add 404 page

---
+++ src/Sorry/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Sorry extends Component {
+  render() {
+    return(
+      <div id="page-not-found">
+        <h2 className="h2-not-found">"Sorry! I'm afraid this page doesn't exist..."</h2>
+        <h1 className="h1-not-found">4<span className="span-not-found">🦎</span>4</h1>
+      </div>
+    )
+  }
+}

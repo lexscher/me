3c376eee34f8617170e2c3d48b624428e788a767
Alexander Schelchere <ascellie@gmail.com>
2019-09-17 23:48:10 -0400

    webpack(development)

---
+++ webpack.dev.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ] // <~ Rules
+  }, // <~ module
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Alexander',
+      inject: false,
+      template: require('html-webpack-template'),
+      bodyHtmlSnippet: '<main class="main" id="root"></main>'
+    })
+  ] // <~ Plugins
+});

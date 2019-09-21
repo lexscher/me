4a2dbd24d9ff5a24d0ae8e6923c2e0dfeff0b332
Alexander Schelchere <ascellie@gmail.com>
2019-09-20 22:49:19 -0400

    files(js): move into "pages" folder

---
+++ src/js/Pages/Archives/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Archives = () => <h1>Archives</h1>
+
+export default Archives;
---
+++ src/js/Pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <h1>
+    <code>Welcome!</code>
+  </h1>
+);
+
+export default Home;

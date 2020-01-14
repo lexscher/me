eaa988b43ce3d85772bf984b2d7ce7d696576a57
Alexander Schelchere <ascellie@gmail.com>
2020-01-14 10:09:29 -0500

    skills(js): complete

--- src/js/Pages/Skills/index.js
+++ src/js/Pages/Skills/index.js
@@ -4,17 +4,35 @@ const Skills = () => {
   let skillsJsx = () => (
     <div id="skills">
       <h1>Skills</h1>
-      <div className="skill-card">
-        <ul>
-          <li>Technical 1</li>
-          <li>Technical 2</li>
-        </ul>
-      </div>
-      <div className="skill-card">
-        <ul>
-          <li>Non-Technical 1</li>
-          <li>Non-Technical 2</li>
-        </ul>
+      <div className="skills-content">
+        <div className="technical-skills-card">
+          <div>
+            <h2>Technical Skills</h2>
+          </div>
+          <ul>
+            <li>JavaScript</li>
+            <li>Ruby, Ruby on Rails</li>
+            <li>Java</li>
+            <li>NodeJS</li>
+            <li>CSS, SASS, SCSS</li>
+            <li>Git</li>
+            <li>React, Redux, React Hooks</li>
+            <li>Webpack</li>
+            <li>Babel</li>
+            <li>Adobe Creative Cloud</li>
+          </ul>
+        </div>
+        <div className="regular-skills-card">
+          <ul>
+            <li>Project Management</li>
+            <li>Communication</li>
+            <li>Collaboration</li>
+            <li>Customer Service</li>
+            <li>Research</li>
+            <li>Troubleshooting</li>
+          </ul>
+          <h2>Non-Technical Skills</h2>
+        </div>
       </div>
     </div>
   );

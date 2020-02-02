f2e072d9e7586762bc118704307f1f1ac67a9012
Alexander Schelchere <ascellie@gmail.com>
2020-02-01 23:41:17 -0500

    js(NavBar): light -> dark nav functionality

--- src/js/NavBar/index.js
+++ src/js/NavBar/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MobileNav, { Dropdown } from "./Mobile";
-import WebNav from './Web';
+import WebNav from "./Web";
 
 const NavBar = () => {
   const [menuBarSelected, toggleMenuBarSelected] = useState(false);
+  const [darkNav, toggleDarkNav] = useState(false);
   // MOBILE NAV
   const mobileNavJsx = (
     <MobileNav
@@ -11,7 +12,7 @@ const NavBar = () => {
       toggleMenuBarSelected={toggleMenuBarSelected}
     />
   );
-
+  // Mobile -- Dropdown
   const dropdownJsx = (
     <Dropdown
       menuBarSelected={menuBarSelected}
@@ -19,12 +20,34 @@ const NavBar = () => {
     />
   );
 
-  // WEB NAV
-  const webNavJsx = <WebNav />
+  // handle scroll
+  const handleScroll = () => {
+    const [client, scrollYWeb, scrollYMobile, webMinWidth] = [
+      window.scrollY, // Position - where the client has scrolled
+      1515, // this is the turning point for the "Web" design
+      2035, // turning point for the "Mobile" design
+      768 // Width of the page determines if we're "Mobile" or "Web"
+    ];
 
+    // Turning point is determined by the width of the screen.
+    // The "Mobile" screen will have a higher turning point.
+    const turningPoint =
+      window.innerWidth > webMinWidth ? scrollYWeb : scrollYMobile;
+
+    // We'll have a dark nav if we've passed the turning point
+    client > turningPoint ? toggleDarkNav(true) : toggleDarkNav(false);
+  };
+  // add event listener to body
+  const addScrollEventToWindow = () =>
+    window.addEventListener("scroll", handleScroll);
+
+  // When the component mounts
+  useEffect(addScrollEventToWindow, []);
+
+  // Nav Bar
   const navJsx = (
-    <div className="nav">
-      {webNavJsx}
+    <div className={`nav${darkNav ? " nav__dark" : ""}`}>
+      <WebNav />
       {mobileNavJsx}
       {menuBarSelected && dropdownJsx}
     </div>
